c2e0d854938574036f2f466ef3fa3c0b715ff7c5
Davi Komura de Castro <davi.komura@gmail.com>
2024-09-22 23:42:21 +0100

    ajustes no footer

--- src/components/Common/Footer/Contacts.jsx
+++ src/components/Common/Footer/Contacts.jsx
@@ -12,7 +12,7 @@ const Contacts = () => {
             <div className="text-center p-8 bg-black">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-4 mb-28">
                     <div className="group relative sm:ml-8">
-                        <h2 className="text-lg md:text-left sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">
+                        <h2 className="text-lg font-bold md:text-left sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">
                             Conecte-se conosco!
                         </h2>
                         <div className="flex justify-center sm:justify-start mt-4 mb-4">
@@ -25,7 +25,7 @@ const Contacts = () => {
                                 className="text-pink-500 hover:text-pink-700 transition-colors duration-300"><FaInstagram size={iconSize} /></a>
                         </div>
                         <div className="mb-4">
-                            <h2 className="text-lg md:text-left sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">
+                            <h2 className="text-lg font-bold md:text-left sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">
                                 Formas de Pagamento
                             </h2>
                             <div className="flex justify-center sm:justify-start mt-4 mb-4">
@@ -35,20 +35,20 @@ const Contacts = () => {
                                 <p className="mr-6 mt-2">
                                     <img src={MasterCard} alt="MasterCard" width={iconSize} height={iconSize} />
                                 </p>
-                                <p className="mr-6 mt-2.5">
+                                <p className="mt-2.5">
                                     <img src={visa} alt="Visa" width={iconSize} height={iconSize} />
                                 </p>
                             </div>
                         </div>
                     </div>
                     <div className="text-left text-white sm:ml-8">
-                        <h2 className="text-center md:text-left text-lg sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">Email</h2>
+                        <h2 className="text-center font-bold md:text-left text-lg sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">Email</h2>
                         <p className="mb-6 text-center md:text-left">maryoliver.mo63@gmail.com</p>
-                        <h2 className="text-center md:text-left text-lg sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">Endereço</h2>
+                        <h2 className="text-center font-bold md:text-left text-lg sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">Endereço</h2>
                         <p className="text-center md:text-left">R. Fé Em Deus, 610 - Quadra 113 - Jardim São Cristovão, São Luís - MA, 65055-190</p>
                     </div>
                     <div className="text-left text-white sm:ml-8">
-                        <h2 className="text-center md:text-left text-lg sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">Horários de Funcionamento </h2>
+                        <h2 className="text-center font-bold md:text-left text-lg sm:text-xl md:text-2xl lg:text-3xl text-white mb-4">Horários de Funcionamento </h2>
                         <p className="text-center md:text-left mb-2">Segunda-Feira : Fechado</p>
                         <p className="text-center md:text-left mb-2">Terça-Feira : 08:00 – 19:00</p>
                         <p className="text-center md:text-left mb-2">Quarta-Feira : 08:00 – 19:00</p>
